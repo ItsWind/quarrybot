0b6e81021659ab9f94a321e2133f12fb3c51f6c8
Windwhistle <stricklandjake63@gmail.com>
2024-08-04 11:33:17 -0500

    Update quarry.lua

--- quarry.lua
+++ quarry.lua
@@ -24,29 +24,6 @@ local currentState = "idle"
 local homeLocation = Config.homeLocation--vector.new(2, 106, 413)
 local chestLocation = Config.chestLocation--vector.new(1, 105, 412)
 
-local CryptoNet = require("cryptoNet")
-function netStart()
-    CryptoNet.host(Config.networkName)
-    for k, v in pairs(Config.networkUsers) do
-        CryptoNet.addUser(k, v)
-    end
-end
-
-function netEvent(event)
-    if event[1] == "encrypted_message" then
-        local socket = event[3]
-        if socket.username ~= nil and Config.networkUsers[socket.username] ~= nil then
-            local message = event[2]
-            print(socket.username .. ": " .. message)
-            -- Process message
-            currentState = message
-        else
-            CryptoNet.send(socket, "I only talk to valid users.")
-        end
-    end
-end
-CryptoNet.startEventLoop(netStart, netEvent)
-
 local currentMiningData = {
     location = vector.new(0,0,0),
     direction = "n",
@@ -301,12 +278,35 @@ local states = {
     end
 }
 
-while true do
-    if states[currentState] ~= nil then
-        states[currentState]()
-    else
-        print("State '" .. currentState .. "' is unknown to me.")
-        currentState = "idle"
+local CryptoNet = require("cryptoNet")
+function netStart()
+    CryptoNet.host(Config.networkName)
+    for k, v in pairs(Config.networkUsers) do
+        CryptoNet.addUser(k, v)
     end
-    sleep(0.1)
+
+    while true do
+        if states[currentState] ~= nil then
+            states[currentState]()
+        else
+            print("State '" .. currentState .. "' is unknown to me.")
+            currentState = "idle"
+        end
+        sleep(0.1)
+    end
 end
+
+function netEvent(event)
+    if event[1] == "encrypted_message" then
+        local socket = event[3]
+        if socket.username ~= nil and Config.networkUsers[socket.username] ~= nil then
+            local message = event[2]
+            print(socket.username .. ": " .. message)
+            -- Process message
+            currentState = message
+        else
+            CryptoNet.send(socket, "I only talk to valid users.")
+        end
+    end
+end
+CryptoNet.startEventLoop(netStart, netEvent)
