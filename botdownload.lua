72502e827c04e425c21064d27c8469550fbb8384
Windwhistle <stricklandjake63@gmail.com>
2024-08-04 11:23:35 -0500

    cryptonet required

--- botdownload.lua
+++ botdownload.lua
@@ -1,3 +1,4 @@
 shell.run("delete", "quarry.lua")
+shell.run("wget", "https://raw.githubusercontent.com/SiliconSloth/CryptoNet/master/cryptoNet.lua", "cryptoNet.lua")
 shell.run("wget", "https://raw.githubusercontent.com/ItsWind/quarrybot/main/quarryConfig.lua", "quarryConfig.lua")
 shell.run("wget", "https://raw.githubusercontent.com/ItsWind/quarrybot/main/quarry.lua", "quarry.lua")
--- controllerdownload.lua
+++ controllerdownload.lua
@@ -1,2 +1,3 @@
 shell.run("delete", "controller.lua")
+shell.run("wget", "https://raw.githubusercontent.com/SiliconSloth/CryptoNet/master/cryptoNet.lua", "cryptoNet.lua")
 shell.run("wget", "https://raw.githubusercontent.com/ItsWind/quarrybot/main/controller.lua", "controller.lua")
