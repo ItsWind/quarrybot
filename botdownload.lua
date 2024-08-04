e5a21a9b042dd86bd9117deeabe1ad25cff4d4b1
Windwhistle <stricklandjake63@gmail.com>
2024-08-04 11:22:24 -0500

    downloads from comp

---
+++ botdownload.lua
@@ -0,0 +1,3 @@
+shell.run("delete", "quarry.lua")
+shell.run("wget", "https://raw.githubusercontent.com/ItsWind/quarrybot/main/quarryConfig.lua", "quarryConfig.lua")
+shell.run("wget", "https://raw.githubusercontent.com/ItsWind/quarrybot/main/quarry.lua", "quarry.lua")
---
+++ controllerdownload.lua
@@ -0,0 +1,2 @@
+shell.run("delete", "controller.lua")
+shell.run("wget", "https://raw.githubusercontent.com/ItsWind/quarrybot/main/controller.lua", "controller.lua")
